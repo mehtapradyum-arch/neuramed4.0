a0128ecffe9d67e466f5e9f299ca6ea223aa71ac
mehtapradyum-arch <mehtapradyum@gmail.com>
2025-11-15 07:37:09 +0400

    Create generate-vapid.ts

---
+++ scripts/generate-vapid.ts
@@ -0,0 +1,4 @@
+import webpush from "web-push";
+const { publicKey, privateKey } = webpush.generateVAPIDKeys();
+console.log("VAPID_PUBLIC_KEY=", publicKey);
+console.log("VAPID_PRIVATE_KEY=", privateKey);
